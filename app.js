513fba1becb823b32f834191d82dd0171a685126
Keith Charlton <kcc5354@psu.edu>
2024-05-14 08:32:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -62,7 +62,6 @@ function isAdmin(req, res, next) {
 app.post('/webhook', (req, res) => {
   const { headers, body } = req;
   if (headers['x-github-event'] === 'push') {
-    // Execute your script to pull the latest code and restart the server
     exec('sh webhook.sh', (error, stdout, stderr) => {
       if (error) {
         console.error(`exec error: ${error}`);
